f83b8477fa93d0c107a8aec67bf5cfd9fae5ea37
Руслан Каписъски <ruslank1@abv.bg>
2025-11-22 07:00:37 +0200

    Show register form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,22 +7,24 @@ import Home from "./components/home/Home";
 import Catalog from "./components/catalog/Catalog";
 import Details from "./components/details/Details";
 import CreateGame from "./components/game-create/CreateGame";
+import Register from "./components/register/Register.jsx";
 
 function App() {
-  return (
-    <>
-      <Header />
+	return (
+		<>
+			<Header />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/games/create" element={<CreateGame />} />
-        <Route path="/games/:gameId/details" element={<Details />} />
-      </Routes>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/catalog" element={<Catalog />} />
+				<Route path="/games/create" element={<CreateGame />} />
+				<Route path="/games/:gameId/details" element={<Details />} />
+				<Route path="/register" element={<Register />} />
+			</Routes>
 
-      <Footer />
-    </>
-  );
+			<Footer />
+		</>
+	);
 }
 
 export default App;
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,24 +1,24 @@
 import { Link } from "react-router";
 
 export default function Header() {
-  return (
-    <header>
-      <nav>
-        <Link className="home" to="/">
-          <img src="./public/images/logo.png" alt="logo" />
-        </Link>
-        <Link to="/catalog">Catalog</Link>
-        {/* <!-- Logged-in users --> */}
-        <div id="user">
-          <Link to="/games/create">Add Game</Link>
-          <Link to="/logout">Logout</Link>
-        </div>
-        {/* <!-- Guest users --> */}
-        <div id="guest">
-          <Link to="/users/login">Login</Link>
-          <Link to="/users/register">Register</Link>
-        </div>
-      </nav>
-    </header>
-  );
+	return (
+		<header>
+			<nav>
+				<Link className="home" to="/">
+					<img src="./public/images/logo.png" alt="logo" />
+				</Link>
+				<Link to="/catalog">Catalog</Link>
+				{/* <!-- Logged-in users --> */}
+				<div id="user">
+					<Link to="/games/create">Add Game</Link>
+					<Link to="/logout">Logout</Link>
+				</div>
+				{/* <!-- Guest users --> */}
+				<div id="guest">
+					<Link to="/login">Login</Link>
+					<Link to="/register">Register</Link>
+				</div>
+			</nav>
+		</header>
+	);
 }
