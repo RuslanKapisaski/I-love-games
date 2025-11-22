86361107be749bf13fd06576259f8871e193014c
Руслан Каписъски <ruslank1@abv.bg>
2025-11-22 12:49:21 +0200

    Use unified approach for control edit form

--- client/src/components/edit/Edit.jsx
+++ client/src/components/edit/Edit.jsx
@@ -1,46 +1,103 @@
-export default function Edit(){
-    return(
-        //  <!-- add Page ( Only for logged-in users ) -->
-        <section id="edit-page">
-            <form id="add-new-game">
-                <div className="container">
+import { useState } from "react";
 
-                    <h1>Edit Game</h1>
+export default function Edit() {
+  const initialValues = {
+    title: "",
+    genre: "",
+    players: "",
+    date: "",
+    imageUrl: "",
+    summary: "",
+  };
 
-                    <div className="form-group-half">
-                        <label htmlFor="gameName">Game Name:</label>
-                        <input type="text" id="gameName" name="gameName" placeholder="Enter game title..."/>
-                    </div>
+  const [values, setValues] = useState(initialValues);
 
-                    <div className="form-group-half">
-                        <label htmlFor="genre">Genre:</label>
-                        <input type="text" id="genre" name="genre" placeholder="Enter game genre..."/>
-                    </div>
+  const changeHandler = (e) => {
+    setValues((state) => ({ ...state, [e.target.name]: [e.target.value] }));
+  };
 
-                    <div className="form-group-half">
-                        <label htmlFor="activePlayers">Active Players:</label>
-                        <input type="number" id="activePlayers" name="activePlayers" min="0" placeholder="0"/>
-                    </div>
+  return (
+    //  <!-- add Page ( Only for logged-in users ) -->
+    <section id="edit-page">
+      <form id="add-new-game">
+        <div className="container">
+          <h1>Edit Game</h1>
 
-                    <div className="form-group-half">
-                        <label htmlFor="releaseDate">Release Date:</label>
-                        <input type="date" id="releaseDate" name="releaseDate"/>
-                    </div>
+          <div className="form-group-half">
+            <label htmlFor="gameName">Game Name:</label>
+            <input
+              type="text"
+              id="gameName"
+              name="title"
+              value={values.title}
+              onChange={changeHandler}
+              placeholder="Enter game title..."
+            />
+          </div>
 
-                    <div className="form-group-full">
-                        <label htmlFor="imageUrl">Image URL:</label>
-                        <input type="text" id="imageUrl" name="imageUrl" placeholder="Enter image URL..."/>
-                    </div>
+          <div className="form-group-half">
+            <label htmlFor="genre">Genre:</label>
+            <input
+              type="text"
+              id="genre"
+              name="genre"
+              value={values.genre}
+              onChange={changeHandler}
+              placeholder="Enter game genre..."
+            />
+          </div>
 
-                    <div className="form-group-full">
-                        <label htmlFor="summary">Summary:</label>
-                        <textarea name="summary" id="summary" rows="5"
-                            placeholder="Write a brief summary..."></textarea>
-                    </div>
+          <div className="form-group-half">
+            <label htmlFor="activePlayers">Active Players:</label>
+            <input
+              type="number"
+              id="activePlayers"
+              name="players"
+              value={values.players}
+              onChange={changeHandler}
+              min="0"
+              placeholder="0"
+            />
+          </div>
 
-                    <input className="btn submit" type="submit" value="EDIT GAME"/>
-                </div>
-            </form>
-        </section>
-    )
+          <div className="form-group-half">
+            <label htmlFor="releaseDate">Release Date:</label>
+            <input
+              type="date"
+              id="releaseDate"
+              name="date"
+              value={values.date}
+              onChange={changeHandler}
+            />
+          </div>
+
+          <div className="form-group-full">
+            <label htmlFor="imageUrl">Image URL:</label>
+            <input
+              type="text"
+              id="imageUrl"
+              name="imageUrl"
+              value={values.imageUrl}
+              onChange={changeHandler}
+              placeholder="Enter image URL..."
+            />
+          </div>
+
+          <div className="form-group-full">
+            <label htmlFor="summary">Summary:</label>
+            <textarea
+              id="summary"
+              rows="5"
+              name="summary"
+              value={values.summary}
+              onChange={changeHandler}
+              placeholder="Write a brief summary..."
+            ></textarea>
+          </div>
+
+          <input className="btn submit" type="submit" value="EDIT GAME" />
+        </div>
+      </form>
+    </section>
+  );
 }
